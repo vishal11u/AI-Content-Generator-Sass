e149f8e4ae8f4ac66686204d7c6a685002e36e45
vishal11u <shitolevishal29@gmail.com>
2025-03-09 14:28:53 +0530

    update path from clert config middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/forum(.*)",
-  "/",
-]);
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware(async (authFn, req) => {
   const auth = await authFn();
