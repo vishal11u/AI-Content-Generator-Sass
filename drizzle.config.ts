f3ec9d3c8675f27b2f3b3de6ab1d9f1c50270074
vishal11u <shitolevishal29@gmail.com>
2025-03-10 23:01:57 -0700

    update import of drizzle api key from .env.local file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ export default {
   schema: "./utils/Schema.tsx",
   dialect: "postgresql",
   dbCredentials: {
-    url: NEXT_PUBLIC_DRIZZLE_DB_KEY,
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_KEY,
   },
 };
 
